3288a90ce57f03dc5d2409e64565c051a49605b9
fresh502 <fresh502@gmail.com>
2023-03-12 18:07:00 +0900

    Validation, Transformer

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -6,6 +7,7 @@ async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('NestJS project')
     .setDescription('NestJS project API description')
@@ -13,6 +15,14 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // ValidationPipe 전역 적용
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // class-transformer 적용
+      transform: true,
+    }),
+  );
 
   await app.listen(port);
   console.info(`listening on port ${port}`);
