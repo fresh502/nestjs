507d4941f7be09a1e66fa2cbe944883140dd7b2a
Mark <mark@quotabook.com>
2023-04-08 09:43:45 +0900

    ch-1 시작

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
-import { Analytics } from './entity/analytics.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Analytics])],
-  controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [AnalyticsService],
 })
